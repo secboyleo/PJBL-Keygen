7a6a3eb3292ae6dcc6c3428442951d8f04672c7c
Pedro Arthur <pedroarthurmelorodrigues@gmail.com>
2025-06-03 21:32:33 -0300

    Adição do último atributo para o requisito

--- Software-real/src/entidades/Usuario.java
+++ Software-real/src/entidades/Usuario.java
@@ -1,15 +1,19 @@
 package entidades;
 
+import java.time.LocalDate;
+
 public class Usuario {
     private String nome;
     private String email;
     private String identificador;
     private String id;
+    private LocalDate dataCadastro;
 
     public Usuario(String nome, String email, String id){
         this.nome = nome;
         this.email = email;
         this.id = id;
+        this.dataCadastro = LocalDate.now();
     }
 
     //o identificador e gerado pelo tamanho do nome + tamanho do email + email
@@ -18,6 +22,10 @@ public class Usuario {
     public String getIdentificador(){
         this.identificador = geraIdentificador();
         return this.identificador;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
     }
 }
 
