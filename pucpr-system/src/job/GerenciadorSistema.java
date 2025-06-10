e6b42645c8ce5386157efba456cdfe9045f7b48a
pedroarthurmelo <pedroarthurmelorodrigues@gmail.com>
2025-06-10 20:59:52 -0300

    adição do throw new das excessoes adicionadas e dos mostrar arrays na main

--- pucpr-system/src/job/GerenciadorSistema.java
+++ pucpr-system/src/job/GerenciadorSistema.java
@@ -4,7 +4,12 @@ import entities.Aluno;
 import entities.Curso;
 import entities.Disciplina;
 import entities.Professor;
+import excessoes.AlunoNaoEncontrado;
+import excessoes.CursoNaoEncontrado;
+import excessoes.DiscplinaNaoEncontrada;
+import excessoes.ProfessorNaoEncontrado;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class GerenciadorSistema {
@@ -13,58 +18,102 @@ public class GerenciadorSistema {
     private ArrayList<Curso> cursos;
     private ArrayList<Disciplina> disciplinas;
 
-    public GerenciadorSistema() {
+    public GerenciadorSistema(){
         this.alunos = new ArrayList<>();
-        this.professores = new ArrayList<>();
+        this.professores =  new ArrayList<>();
         this.cursos = new ArrayList<>();
         this.disciplinas = new ArrayList<>();
     }
 
-    public void cadastrarAluno(Aluno aluno) {
+    public void cadastrarAluno(Aluno aluno){
         alunos.add(aluno);
     }
 
-    public void cadastrarProfessor(Professor professor) {
+    public void cadastrarProfessor(Professor professor){
         professores.add(professor);
     }
 
-    public void criarCurso(Curso curso) {
+    public void criarCurso(Curso curso){
         cursos.add(curso);
     }
 
-    public void criarDisciplina(Disciplina disciplina) {
+    public void criarDisciplina(Disciplina disciplina){
         disciplinas.add(disciplina);
     }
 
-    public String buscarProfessor(String cpf) {
-        for (Professor professor : professores) {
-            if (professor.getCpf().equals(cpf)) {
-                return professor.exibirInformacoes();
-            }
+    //busca aluno, professor curso e disciplinas---------------------------------------------------------------------
+    public String buscarAluno(String matricula) throws AlunoNaoEncontrado {
+        for(Aluno aluno : alunos){
+            if(aluno.getMatricula().equals(matricula)){ return aluno.exibirInformacoes(); }
         }
 
-        return cpf;
+        throw new AlunoNaoEncontrado("Aluno com matricula: " + matricula + ", nao foi encontrado");
     }
 
-    public String buscaCurso(String codigo){
-        for (Curso curso : cursos) {
-            if (curso.getCodigo().equals(codigo)) {
-                return "Curso:" + curso.getNome() + " | Codigo: " + curso.getCodigo();
-            }
+    public String buscarProfessor(String cpf) throws ProfessorNaoEncontrado {
+        for(Professor professor : professores){
+            if(professor.getCpf().equals(cpf)){ return professor.exibirInformacoes(); }
         }
 
-        return codigo;
+        throw new ProfessorNaoEncontrado("Professor com cpf: " + cpf + ", nao foi encontrado");
     }
 
-    public String buscaDiscplina(String codigo) {
-        for (Disciplina disciplina : disciplinas) {
-            if (disciplina.getCodigo().equals(codigo)) {
-                return "Discplina:" + disciplina.getNome() + " | Codigo: " + disciplina.getCodigo();
-            }
+    public String buscaCurso(String codigo) throws CursoNaoEncontrado {
+        for(Curso curso : cursos){
+            if(curso.getCodigo().equals(codigo)){ return "Curso:" + curso.getNome() + " | Codigo: " + curso.getCodigo(); }
         }
 
-
-        return codigo;
+        throw new CursoNaoEncontrado("Curso com codigo: " + codigo + ", nao foi encontrado");
     }
 
+    public String buscaDiscplina(String codigo) throws DiscplinaNaoEncontrada {
+        for(Disciplina disciplina : disciplinas){
+            if(disciplina.getCodigo().equals(codigo)){ return "Discplina:" + disciplina.getNome() + " | Codigo: " + disciplina.getCodigo();}
+        }
+
+        throw new DiscplinaNaoEncontrada("Discplina com o codigo: " + codigo + ", nao encontrada");
+    }
+
+    //mostrar arrays-------------------------------------------------------------------------------------------------------------------
+    public void mostrarAlunos(){
+        System.out.println("-----------ALUNOS------------");
+        for(Aluno aluno : alunos){
+            System.out.println(aluno.exibirInformacoes());
+            System.out.println();
+        }
+
+        System.out.println("----------------------------------");
+    }
+
+    public void mostrarProfessores(){
+        System.out.println("-----------Professores------------");
+        for(Professor professor : professores){
+            System.out.println(professor.exibirInformacoes());
+            System.out.println();
+        }
+
+        System.out.println("----------------------------------");
+    }
+
+    public void mostrarCursos(){
+        System.out.println("-------------Cursos-------------");
+        for(Curso curso : cursos){
+            System.out.println(curso.toString());
+            System.out.println();
+        }
+    }
+
+    public void mostrarDisciplinas(){
+        System.out.println("--------------Disciplinas----------------");
+        for(Disciplina disciplina : disciplinas){
+            System.out.println(disciplina.toString());
+            System.out.println();
+        }
+    }
+
+    //metodos get dos arrays (pode ser util mais para frente)
+    public ArrayList<Aluno> getAlunos() { return this.alunos;}
+    public ArrayList<Professor> getProfessores() {return professores;}
+    public ArrayList<Curso> getCursos() {return cursos;}
+    public ArrayList<Disciplina> getDisciplinas() {return disciplinas;}
 }
